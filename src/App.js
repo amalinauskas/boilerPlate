7446478623a087d231a5cfd2196e16d5dca40997
Your Name <you@example.com>
2022-06-07 22:14:02 +0300

    App.js atvaizdavimas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CardList from "./components/CardList/CardList";
 
 const App = () => {
-  return <div>App</div>;
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    fetch("https://golden-whispering-show.glitch.me/")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <main>
+      <h1>Products</h1>
+      {!products && <div>Loading...</div>}
+      {products && <CardList products={products} />}
+    </main>
+  );
 };
 
 export default App;
