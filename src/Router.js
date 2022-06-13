d5832c9ec826c565608385fd241ae71fcde151be
Your Name <you@example.com>
2022-06-13 19:37:35 +0300

    router fix

--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
-import About from "./pages/About";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
 
 const Router = () => {
   return (
